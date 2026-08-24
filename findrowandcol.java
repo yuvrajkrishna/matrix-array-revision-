32a4798f0e02556d7078a7dcd6b79709846b9f78
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-24 21:07:00 +0530

    print a matrix

--- findrowandcol.java
+++ findrowandcol.java
@@ -1,6 +1,6 @@
 public class findrowandcol{
     public static void main(String[] args) {
-        int arr[][] = {{1,2,3,4},{2,3,4,5},{6,7,8,9}};
+        int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
         System.out.print("Row :");
         System.out.println(arr.length);
         System.out.print("Col :");
