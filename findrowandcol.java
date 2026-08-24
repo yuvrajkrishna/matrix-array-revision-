cb01ff4d8f36327a0af008d30e9e3d3f2be014a0
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-24 20:57:25 +0530

    find row and col

---
+++ findrowandcol.java
@@ -0,0 +1,9 @@
+public class findrowandcol{
+    public static void main(String[] args) {
+        int arr[][] = {{1,2,3,4},{2,3,4,5},{6,7,8,9}};
+        System.out.print("Row :");
+        System.out.println(arr.length);
+        System.out.print("Col :");
+        System.out.println(arr[0].length);
+    }
+}
