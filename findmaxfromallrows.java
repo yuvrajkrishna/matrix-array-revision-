588277452a982279d88eb68431e476bd7c85341a
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-08-24 21:35:40 +0530

    find max from all rows

---
+++ findmaxfromallrows.java
@@ -0,0 +1,14 @@
+public class findmaxfromallrows {
+    public static void main(String[] args) {
+        int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
+        for(int i = 0 ; i < arr.length ; i++){
+            int max = arr[i][0];
+            for(int j = 1; j < arr[0].length ; j++){
+                if(arr[i][j] > max){
+                    max = arr[i][j];
+                }
+            }
+            System.out.println(max);
+        }
+    }
+}
